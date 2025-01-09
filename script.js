443dd752d3b6a1adca825997d3110226536f2f19
neycato <ney.cantillo@cun.edu.co>
2025-01-08 20:26:09 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+function mostrarInfo(lugar) {
+    let infoText = document.getElementById('info-' + lugar);
+    
+    // Información de cada maravilla
+    const infos = {
+        'muralla': 'La Gran Muralla China es una serie de fortificaciones construidas para proteger el imperio chino. Su longitud supera los 21,000 kilómetros, y fue construida entre los siglos V a.C. y XVI.',
+        'petra': 'Petra, ubicada en Jordania, es una antigua ciudad famosa por su arquitectura excavada en roca. Fue la capital del reino nabateo y es conocida por sus impresionantes estructuras, como el Tesoro.',
+        'cristo': 'El Cristo Redentor es una enorme estatua de Jesucristo ubicada en Río de Janeiro, Brasil. Mide 30 metros de altura y es uno de los símbolos más representativos de Brasil.',
+        'machu': 'Machu Pic
